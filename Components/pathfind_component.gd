975e4663914b3397cf672d2c468a1637aff26706
Crabby Disk <yfmao2010@hotmail.com>
2024-03-24 09:53:37 -0400

    Add Dummy and AI

--- Components/pathfind_component.gd
+++ Components/pathfind_component.gd
@@ -2,8 +2,14 @@ extends NavigationAgent2D
 class_name PathfindComponent
 
 @export var target : Node2D
+@export var parent: Node2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_dir():
+	var direction: Vector2
+	target_position = target.global_position
+	if target.global_position.distance_to(parent.global_position) > 80:
+		direction = get_next_path_position() - parent.global_position
+	else:
+		direction = parent.global_position - get_next_path_position()
+	direction = direction.normalized()
+	return direction
--- Enemies/dummy.gd
+++ Enemies/dummy.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+@export var player: CharacterBody2D
+
 @onready var pathfinding = $PathfindComponent
-const SPEED = 300.0
-func _on_ready():
-	
+const SPEED = 150.0
+func _ready():
+	pathfinding.target = player
+	pathfinding.parent = self
+
 func _physics_process(delta):
-	pathfinding.target = 
+	velocity = lerp(velocity, pathfinding.get_dir() * SPEED, .1)
 	move_and_slide()
